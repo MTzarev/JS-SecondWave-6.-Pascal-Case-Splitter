47d67fa37440fba1ada63aba12c13d73c29d9f2c
Miroslav Tsarev <mtzarev@gmail.com>
2024-09-14 21:57:19 +0300

    Add files via upload

---
+++ 6. Pascal-Case Splitter.js
@@ -0,0 +1,14 @@
+function pascalCaseSplitter(string) {
+    let result = [];
+    let first = 0;
+    for (let i = 1; i < string.length; i++) {
+        if (string[i] === string[i].toUpperCase()) {
+            result.push(string.slice(first, i));
+            first = i;
+        }
+    }
+    result.push(string.slice(first));
+    console.log(result.join(`, `));
+
+}
+pascalCaseSplitter(`SplitMeIfYouCanHaHaYouCantOrYouCan`);
